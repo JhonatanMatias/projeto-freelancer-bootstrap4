9b5a22f28c24be1bca982acf76dda534ca98c1ef
Jhonatan Oliveira <jhonatanmatiaso@gmail.com>
2019-02-16 19:52:59 -0200

    -Adicionado efeito de rolagem suave com jquery-easing

--- js/freelancer.js
+++ js/freelancer.js
@@ -1,6 +1,24 @@
-(function($) {
+$(document).ready(function() {
 
-	"use strict";
+
+	//rolagem suave com jquery Easing
+	$('a.js-scroll-trigger[href*="#"]:not[href="#"]').click(function(){
+
+		if(location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname){
+
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if(target.length){
+
+				$('html','body').animate({
+
+					scrollTop: (target.offset().top - 70)
+				},1000,"EaseInOutExpo");
+
+				return false;
+			}
+		}
+	})
 
 	//Modal popup$ (function() {
 	$('.portfolio-item').magnificPopup({
@@ -15,4 +33,4 @@
     	e.preventDefault();
    		$.magnificPopup.close();
   });
-})(JQuery);
+});
