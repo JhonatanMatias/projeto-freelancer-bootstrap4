d15b781bb20f9d7753d6df2047c68563931cdfe7
Jhonatan Oliveira <jhonatanmatiaso@gmail.com>
2019-02-17 14:10:10 -0300

    - Adicionado a funcionalidade de flutuamento de rotulo acima dos objetos de formúlario quando os mesmos são modificados

--- js/freelancer.js
+++ js/freelancer.js
@@ -81,4 +81,21 @@ $(document).ready(function() {
     	e.preventDefault();
    		$.magnificPopup.close();
   });
+
+	 //Flutuando os rotulos de cabeçalho acima do objetos de formúlario de contato.
+	 $(function(){
+
+	 	$("body").on("input propertychange",'.floating-label-form-group',function(e){
+
+	 		$(this).toggleClass('floating-label-form-group-with-value',!!$(e.target).val());
+	 	}).on("focus",".floating-label-form-group",function(){
+
+	 		$(this).addClass('floating-label-form-group-with-focus');
+	 	}).on("blur",".floating-label-form-group",function(){
+
+	 		$(this).removeClass('floating-label-form-group-with-focus');
+	 	})
+	 });
 });
+
+
